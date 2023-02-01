c36ac7f9839b817f424818090cc6ef0329b46842
Mam Tan <mamtatank@Mamtas-MacBook-Pro.local>
2023-02-01 17:39:53 +0000

    added code to for localstorage

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -13,82 +13,104 @@ let timerId;
 let index = 0;
 let currentQuestion;
 function beginQuiz() {
-   
-     // start timer
-    timerId = setInterval(oneSecFunction, 1000);
-    timeEl.textContent = timeRemaining;
-    //hide start div / start page
-    startScreen.setAttribute("class", "hide");
-    //unhide questions div
-    questionsDiv.removeAttribute("class");
-    // Call a function to Show the first question
-    showQuestion();
+  // start timer
+  timerId = setInterval(oneSecFunction, 1000);
+  timeEl.textContent = timeRemaining;
+  //hide start div / start page
+  startScreen.setAttribute("class", "hide");
+  //unhide questions div
+  questionsDiv.removeAttribute("class");
+  // Call a function to Show the first question
+  showQuestion();
 }
 
-function showQuestion(){
-    //to be compelted
-    console.log("ShowQuestion");
-    currentQuestion = questions[index];
-    questionTitle.textContent = currentQuestion.question;
-    choicesEl.innerHTML = "";
-    currentQuestion.choices.forEach(function(choice,i){
-        let btn = document.createElement("button");
-        btn.setAttribute("class", "choice");
-        btn.setAttribute("value", choice);
+function showQuestion() {
+  //to be compelted
+  console.log("ShowQuestion");
+  currentQuestion = questions[index];
+  questionTitle.textContent = currentQuestion.question;
+  choicesEl.innerHTML = "";
+  currentQuestion.choices.forEach(function (choice, i) {
+    let btn = document.createElement("button");
+    btn.setAttribute("class", "choice");
+    btn.setAttribute("value", choice);
 
-        btn.textContent = i+1+". " + choice;
-        btn.onclick = handleAnswerChoice;
+    btn.textContent = i + 1 + ". " + choice;
+    btn.onclick = handleAnswerChoice;
 
-        choicesEl.appendChild(btn);
-    });
+    choicesEl.appendChild(btn);
+  });
 }
 
-function handleAnswerChoice(e){
-    //if user select wrong answer reduce timer by 15
-    // inform user its wrong asnwer
-    //else inform user right answer
-    //increament index
-    console.log("Handle Answer Choice");
-    let btn = e.target;
-    if (!btn.matches(".choice")) {
+function handleAnswerChoice(e) {
+  //if user select wrong answer reduce timer by 15
+  // inform user its wrong asnwer
+  //else inform user right answer
+  //increament index
+  console.log("Handle Answer Choice");
+  let btn = e.target;
+  if (!btn.matches(".choice")) {
     return;
+  }
+  console.log("Button.value = ", btn.value);
+  console.log("Correct Asnwer = ", currentQuestion.correctAnswer);
+  if (btn.value !== currentQuestion.correctAnswer) {
+    timeRemaining -= 15;
+    if (timeRemaining < 0) {
+      timeRemaining = 0;
     }
-    console.log("Button.value = " , btn.value);
-    console.log("Correct Asnwer = " , currentQuestion.correctAnswer);
-    if (btn.value !== currentQuestion.correctAnswer) {
-        timeRemaining -= 15;
-        if (timeRemaining < 0) {
-            timeRemaining = 0;
-        }
-        timeEl.textContent = timeRemaining;
-        alert("Wrong answer Press Ok to continue");
-    } else {
-        alert("Correct answer Press Ok to continue");
-    } 
-    index++;
-    if (timeRemaining <= 0 || index === questions.length) {
-        endQuiz();
-    } else {
-        showQuestion();
-    }
+    timeEl.textContent = timeRemaining;
+    alert("Wrong answer Press Ok to continue");
+  } else {
+    alert("Correct answer Press Ok to continue");
+  }
+  index++;
+  if (timeRemaining <= 0 || index === questions.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
 }
 
 function oneSecFunction() {
-    timeRemaining -- ;
-    timeEl.textContent = timeRemaining;
-    if (timeRemaining <= 0) {
-        endQuiz();
-    }
-    }
-    function endQuiz() {
-        //to be completed
-        clearInterval(timerId); 
-        let endScreen = document.getElementById("end-screen");
-        endScreen.removeAttribute("class");
+  timeRemaining--;
+  timeEl.textContent = timeRemaining;
+  if (timeRemaining <= 0) {
+    endQuiz();
+  }
+}
+function endQuiz() {
+  //to be completed
+  clearInterval(timerId);
+  let endScreen = document.getElementById("end-screen");
+  endScreen.removeAttribute("class");
 
-        let finalScore = document.getElementById("final-score");
-        finalScore.textContent = timeRemaining;
-        questionsDiv.setAttribute("class", "hide");
-    }
+  let finalScore = document.getElementById("final-score");
+  finalScore.textContent = timeRemaining;
+  questionsDiv.setAttribute("class", "hide");
+}
 
-    // \\\
+let submitBtn = document.getElementById("submit");
+let initialsEl = document.getElementById("initials");
+
+submitBtn.onclick = saveScore;
+
+function saveScore () {
+    let initials = initialsEl.value.trim();
+
+    if (initials === "") {
+        return;
+    }
+    let topScores = JSON.parse(window.localStorage.getItem("topScores")) || [];
+
+    let playerScore = {
+        score: timeRemaining,
+        initials: initials,
+    };
+
+    topScores.push(playerScore);
+    localStorage.setItem("topScores", JSON.stringify(topScores));
+
+    window.location.href = "highscores.html"
+}
+
